1df1ae273f8f107912e2499c8b1c00044feb4998
saleh kanaan <saleh.kanaan322@gmail.com>
2024-08-16 17:29:28 +0300

    m

--- src/usefetch.js
+++ src/usefetch.js
@@ -1,33 +1,58 @@
 import { useState, useEffect } from "react";
+import axios from 'axios';
 const useFetch = (url) => { //fetching different data other than blogs
     const [isPending, setIsPending] = useState(true);
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
+//     useEffect(() => {
+//         const abortCont =new AbortController();
+//         setTimeout(() => {//message timeout
+//             fetch(url,{signal:abortCont.signal})//json fetch
+//                 .then(res => {
+//                     if (!res.ok) {
+//                         throw Error('could not fetch  the data for that resource')
+//                     }
+//                     return res.json();
+//                 })
+//                 .then(data => {
+//                     setData(data);
+//                     setIsPending(false);
+//                     setError(null);
+//                 }).catch(err => {
+//                     if(err.name==='AbortError'){
+//                         console.log('fetch aborted');
+//                     }else{
+//                     setError(err.message);
+//                     setIsPending(false);
+//                     }
+//                 })
+//         }, 1000);
+// return ()=>{
+//    abortCont.abort(); 
+// }
+//     }, [url]); 
     useEffect(() => {
-        const abortCont =new AbortController();
-        setTimeout(() => {  //message timeout
-            fetch(url,{signal:abortCont.signal})//json fetch
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('could not fetch  the data for that resource')
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setData(data);
+       
+        const cancelToken = axios.CancelToken.source()
+        setTimeout(() => {//message timeout
+            axios.get(url,{cancelToken:cancelToken.token})//json fetch
+                .then((res) => {
+                    setData(res.data);
                     setIsPending(false);
                     setError(null);
                 }).catch(err => {
-                    if(err.name==='AbortError'){
+                    if (axios.isCancel(err)) {
                         console.log('fetch aborted');
-                    }else{
-                    setError(err.message);
-                    setIsPending(false);
+                    } else {
+                        setError(err.message);
+                        setIsPending(false);
                     }
                 })
         }, 1000);
-return ()=>abortCont.abort();
-    }, [url]);  //[] is for dependencies
+        return () => {
+            cancelToken.cancel();
+        }
+    }, [url]); //[] is for dependencies
     return { data, isPending, error }
 }
 export default useFetch;
